8549c41cdc635955c252d75ca3f9fcb2cbd30c9a
andrey <andreyabadsalinas2@gmail.com>
2021-04-07 21:36:22 -0500

    boton de navegacion agregado

--- app/src/main/java/com/example/cuidatv3/fragments/Fragment_Calculadoras.kt
+++ app/src/main/java/com/example/cuidatv3/fragments/Fragment_Calculadoras.kt
@@ -1,10 +1,11 @@
-package com.example.cuidatv3
+package com.example.cuidatv3.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.cuidatv3.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Fragment_Ejercicios.newInstance] factory method to
+ * Use the [Fragment_Calculadoras.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fragment_Ejercicios : Fragment() {
+class Fragment_Calculadoras : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class Fragment_Ejercicios : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__ejercicios, container, false)
+        return inflater.inflate(R.layout.fragment__calculadoras, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class Fragment_Ejercicios : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment_Ejercicios.
+         * @return A new instance of fragment Fragment_Calculadoras.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Fragment_Ejercicios().apply {
+            Fragment_Calculadoras().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/cuidatv3/fragments/Fragment_TablaDeAlimentos.kt
+++ app/src/main/java/com/example/cuidatv3/fragments/Fragment_TablaDeAlimentos.kt
@@ -1,10 +1,11 @@
-package com.example.cuidatv3
+package com.example.cuidatv3.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.cuidatv3.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Fragment_Calculadoras.newInstance] factory method to
+ * Use the [Fragment_TablaDeAlimentos.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fragment_Calculadoras : Fragment() {
+class Fragment_TablaDeAlimentos : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class Fragment_Calculadoras : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment__calculadoras, container, false)
+        return inflater.inflate(R.layout.fragment__tabla_de_alimentos, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class Fragment_Calculadoras : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Fragment_Calculadoras.
+         * @return A new instance of fragment Fragment_TablaDeAlimentos.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Fragment_Calculadoras().apply {
+            Fragment_TablaDeAlimentos().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
